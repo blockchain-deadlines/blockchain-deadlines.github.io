1d7cecc7a1fb1c27c82ffd78f89814852d765b33
Joachim Neu <jneu@a16z.com>
2026-03-10 13:46:50 -0400

    .

--- compress-conferences.sh
+++ compress-conferences.sh
@@ -4,5 +4,7 @@
 for f in _data/conferences_raw/*.yml; do
   cat "$f" >> _data/conferences.yml
   # Append a newline if the file doesn't end with one
-  [ -s "$f" ] && [ "$(tail -c1 "$f" | wc -l)" -eq 0 ] && echo >> _data/conferences.yml
+  if [ -s "$f" ] && [ "$(tail -c1 "$f" | wc -l)" -eq 0 ]; then
+    echo >> _data/conferences.yml
+  fi
 done
