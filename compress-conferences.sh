be71aa206c4e46dee6d85ce4c1b45869ffd7b569
Joachim Neu <jneu@a16z.com>
2026-03-10 13:45:03 -0400

    .

--- compress-conferences.sh
+++ compress-conferences.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-cat _data/conferences_raw/*.yml > _data/conferences.yml
+#!/bin/bash
+
+> _data/conferences.yml
+for f in _data/conferences_raw/*.yml; do
+  cat "$f" >> _data/conferences.yml
+  # Append a newline if the file doesn't end with one
+  [ -s "$f" ] && [ "$(tail -c1 "$f" | wc -l)" -eq 0 ] && echo >> _data/conferences.yml
+done
